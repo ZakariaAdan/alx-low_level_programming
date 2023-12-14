d398de7d9befc8ba66bf2da17d62bb3744118be4
ZakariaAdan <ZakariaAdan0337@gmail.com>
2023-12-14 03:14:24 -0800

    question 04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,6 +11,6 @@ int main(void)
 	 * "Programming is like building a multilingual puzzle"
 	 * followed by a new line.
 	 */
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle,\n");
 	return (0);
 }
