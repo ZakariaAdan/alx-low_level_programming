005ec3eb1c3c8065a13bdd8f171070e68ef97692
ZakariaAdan <ZakariaAdan0337@gmail.com>
2023-12-19 06:39:09 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,31 +5,26 @@
 /**
  * main - Entry point
  *
- * Description: Generates a random number, prints it, and classifies its sign
+ * Description: Generates a random number, prints it,
+ *              and classifies its sign
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    printf("%d ", n); /* Prints the random number generated */
+	printf("%d ", n); /* Prints the random number generated */
 
-    if (n > 0)
-    {
-        printf("is positive\n");
-    }
-    else if (n < 0)
-    {
-        printf("is negative\n");
-    }
-    else
-    {
-        printf("is zero\n");
-    }
+	if (n > 0)
+		printf("is positive\n");
+	else if (n < 0)
+		printf("is negative\n");
+	else
+		printf("is zero\n");
 
-    return (0);
+	return (0);
 }
