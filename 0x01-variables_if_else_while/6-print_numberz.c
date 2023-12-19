8252388df903675c77591f9af1e053646426bf01
ZakariaAdan <ZakariaAdan0337@gmail.com>
2023-12-19 06:53:07 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,17 @@
 
 int main(void)
 {
-	/* Using putchar to print digits from 0 to 9 */
-	putchar('0' + 0); /* '0' + 0 equals '0' */
-	putchar('0' + 1); /* '0' + 1 equals '1' */
-	putchar('0' + 2); /* '0' + 2 equals '2' */
-	putchar('0' + 3); /* '0' + 3 equals '3' */
-	putchar('0' + 4); /* '0' + 4 equals '4' */
-	putchar('0' + 5); /* '0' + 5 equals '5' */
-	putchar('0' + 6); /* '0' + 6 equals '6' */
-	putchar('0' + 7); /* '0' + 7 equals '7' */
-	putchar('0' + 8); /* '0' + 8 equals '8' */
-	putchar('0' + 9); /* '0' + 9 equals '9' */
-	
-	putchar('\n'); /* New line after printing the digits */
-	
-	return (0);
+    putchar('0'); /* Using putchar to print '0' */
+    putchar('1'); /* Using putchar to print '1' */
+    putchar('2'); /* Using putchar to print '2' */
+    putchar('3'); /* Using putchar to print '3' */
+    putchar('4'); /* Using putchar to print '4' */
+    putchar('5'); /* Using putchar to print '5' */
+    putchar('6'); /* Using putchar to print '6' */
+    putchar('7'); /* Using putchar to print '7' */
+    putchar('8'); /* Using putchar to print '8' */
+    putchar('9'); /* Using putchar to print '9' */
+    putchar('\n'); /* New line after printing the digits */
+
+    return (0);
 }
