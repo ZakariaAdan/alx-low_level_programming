47bb5e556fe84594f094a7494edef691609c4a51
ZakariaAdan <ZakariaAdan0337@gmail.com>
2023-12-19 04:50:25 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char letter = 'z'; /* Starting letter: 'z' */
+	char letter = 'z'; /* Starting letter: 'z' */
 
-    while (letter >= 'a') /* Loop from 'z' down to 'a' */
-    {
-        putchar(letter); /* Print the current letter */
-        letter--; /* Move to the previous letter */
-    }
+	while (letter >= 'a') /* Loop from 'z' down to 'a' */
+	{
+		putchar(letter); /* Print the current letter */
+		letter--; /* Move to the previous letter */
+	}
 
-    putchar('\n'); /* Print a new line */
+	putchar('\n'); /* Print a new line */
 
-    return 0;
+	return (0);
 }
