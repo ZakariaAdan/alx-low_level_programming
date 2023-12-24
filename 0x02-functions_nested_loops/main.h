a90000b048460f9e348449f00159ef61da7110fd
ZakariaAdan <ZakariaAdan0337@gmail.com>
2023-12-24 04:42:53 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,6 @@
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -15,5 +14,6 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
 
