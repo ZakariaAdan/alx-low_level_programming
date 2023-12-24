36b0287eeecb03a0256d77bda0cf4093b1d7cdfd
ZakariaAdan <ZakariaAdan0337@gmail.com>
2023-12-24 03:55:20 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line in the terminal
+ * @n: number of times to print '_'
+ */
+void print_line(int n)
+{
+    int i;
+
+    if (n <= 0) {
+        _putchar('\n');
+    } else {
+        for (i = 0; i < n; i++) {
+            _putchar('_');
+        }
+        _putchar('\n');
+    }
+}
