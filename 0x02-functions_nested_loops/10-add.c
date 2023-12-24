e045e35bbf671be52c9de5eefdc20256bfec6ec6
ZakariaAdan <ZakariaAdan0337@gmail.com>
2023-12-24 05:10:25 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
+
+
+
+
+Question 10
+#include "main.h"
+
+
+/**
+ * add - entry point
+ *
+ *Description: function that adds two integers and
+ * returns the result.
+ *
+ * @n1: integer on
+ * @n2: integer two
+ *
+ * Return:sum.
+ *
+ */
+
+
+int add(int n1, int n2)
+{
+	int sum;
+
+
+	sum = n1 + n2;
+	return (sum);
+}
