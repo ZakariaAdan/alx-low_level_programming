c9f6e2e09d334e4f6449caf224ca8d75ab353d52
ZakariaAdan <ZakariaAdan0337@gmail.com>
2024-01-11 07:10:05 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: String to be printed
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2; /* Increment by 2 to skip every other character */
+	}
+
+	_putchar('\n');
+}
